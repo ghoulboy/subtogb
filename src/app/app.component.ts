ebc38278f82151fcbb15412309b172a6bce52b67
ghoulboy <gkatsaros16@gmail.com>
2020-01-16 00:53:03 -0500

    added edit stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { timer, BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,90 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'subtogb';
+  key;
+  key2;
+  isRunning = false;
+  time;
+  source;
+  sourceSub;
+  timerSub;
+  timerArray: BehaviorSubject<Array<any>> = new BehaviorSubject([]);
+  sum;
+  avg;
+  edit: any = {};
+  isReady = false;
+
+  ngOnInit() {
+    this.timerSub = this.timerArray.subscribe(x => {
+      this.sum = x.reduce((a, b) => a + b, 0);
+      this.avg = (this.sum / x.length) || 0;
+    })
+  }
+
+  toggleTimer() {
+    this.isRunning = !this.isRunning;
+  }
+
+  @HostListener('document:keypress', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) { 
+    if (this.isReady) {
+      if(!this.key2 && this.key == event.key) {
+        if (!this.isRunning) {
+          this.isRunning = true;
+          this.source = timer(0,0);
+          this.sourceSub = this.source.subscribe(x => this.time = x);
+        } else {
+          this.isRunning = false;
+          this.addData(this.time)
+          this.sourceSub.unsubscribe();
+        }
+      } else if (this.key2) {
+        if(this.key == event.key && !this.isRunning) {
+          this.isRunning = true;
+          this.source = timer(0,0);
+          this.sourceSub = this.source.subscribe(x => this.time = x);
+        } else if (this.key2 == event.key && this.isRunning) {
+          this.isRunning = false;
+          this.addData(this.time)
+          this.sourceSub.unsubscribe();
+        }
+      }
+
+    }
+  }
+
+  ready() {
+    this.isReady = true
+  }
+
+  reset() {
+    this.key = null;
+    this.key2 = null;
+  }
+
+  setKey1(event: KeyboardEvent) {
+    this.key = event.key;
+  }
+
+  setKey2(event: KeyboardEvent) {
+    this.key2 = event.key;
+  }
+
+  clearKey2() {
+    this.key2 = null;
+  }
+
+  addData(dataObj) {
+    const currentValue = this.timerArray.value;
+    const updatedValue = [...currentValue, dataObj];
+    this.timerArray.next(updatedValue);
+  }
+
+  resetKey() {
+    this.key = '';
+  }
+
+  ngOnDestroy() {
+    this.timerSub.unsubscribe()
+  }
 }
